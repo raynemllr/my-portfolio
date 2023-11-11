cbeb6cf19c5418e49d0bbf436de407de6f68c6c1
Rayne Mallari <raynemllr.5@gmail.com>
2023-11-11 15:48:46 +0800

    Add topbar data constants

---
+++ constants/index.ts
@@ -0,0 +1,18 @@
+export const topbarData = [
+  {
+    name: "Home",
+    link: "#home",
+  },
+  {
+    name: "About",
+    link: "#about",
+  },
+  {
+    name: "Projects",
+    link: "#projects",
+  },
+  {
+    name: "Contact",
+    link: "#contact",
+  },
+];
