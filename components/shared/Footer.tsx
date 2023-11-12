df3c438b1d620e8d4d9cb20ee83112851f0e1c9e
Rayne Mallari <raynemllr.5@gmail.com>
2023-11-12 21:43:49 +0800

    Udpate footer width

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="flex flex-col items-center justify-between gap-y-6 md:w-2/3 md:flex-row">
+      <div className="flex w-[90%] flex-col items-center justify-between gap-y-6 md:w-2/3 md:flex-row">
         <h3 className="text-center text-lg font-extrabold">
           © 2023 Rayne Mallari. All rights reserved.
         </h3>
