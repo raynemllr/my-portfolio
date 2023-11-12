ab99db957c51804b03732c0c015adfdb08f3c551
Rayne Mallari <raynemllr.5@gmail.com>
2023-11-12 21:22:36 +0800

    Add RehydrationWrapper for hydration issues

--- lib/Provider.tsx
+++ lib/Provider.tsx
@@ -1,15 +1,27 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
 
 type ProviderProps = {
   children: React.ReactNode;
 };
 
+const RehydrationWrapper = ({ children }: ProviderProps) => {
+  // * To avoid hydration issues
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  return <>{children}</>;
+};
+
 export default function Provider({ children }: ProviderProps) {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      {children}
-    </ThemeProvider>
+    <RehydrationWrapper>
+      <ThemeProvider enableSystem={true} attribute="class">
+        {children}
+      </ThemeProvider>
+    </RehydrationWrapper>
   );
 }
