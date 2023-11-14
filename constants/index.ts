3c9df45ded6d3c6fe7504bbf28caaafe8b806d3f
Rayne Mallari <raynemllr.5@gmail.com>
2023-11-14 14:45:13 +0800

    Add techStackIcons data

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-import { Linkedin, Github } from "lucide-react";
+import { Linkedin, Github, Facebook } from "lucide-react";
 
 export const topbarData = [
   {
@@ -28,5 +28,52 @@ export const personalLinks = [
   {
     icon: Github,
     link: "https://github.com/raynemllr",
+  },
+  {
+    icon: Facebook,
+    link: "https://www.facebook.com/rayne.mallari/",
+  },
+];
+
+// * for Hero section - Tech stack
+export const techStackIcons = [
+  {
+    name: "html",
+    icon: "/assets/dev-icons/html.svg",
+  },
+  {
+    name: "css",
+    icon: "/assets/dev-icons/css.svg",
+  },
+  {
+    name: "js",
+    icon: "/assets/dev-icons/js.svg",
+  },
+  {
+    name: "ts",
+    icon: "/assets/dev-icons/ts.svg",
+  },
+  {
+    name: "react",
+    icon: "/assets/dev-icons/react.svg",
+  },
+  {
+    name: "nextjs",
+    icon: {
+      light: "/assets/dev-icons/nextjs.svg",
+      dark: "/assets/dev-icons/nextjs-dark.svg",
+    },
+  },
+  {
+    name: "mongodb",
+    icon: "/assets/dev-icons/mongodb.svg",
+  },
+  {
+    name: "tailwind",
+    icon: "/assets/dev-icons/tailwind.svg",
+  },
+  {
+    name: "mui",
+    icon: "/assets/dev-icons/mui.svg",
   },
 ];
