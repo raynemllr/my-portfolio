b1288e96a9fed4d3554daa73984ffaa4fb1add16
Rayne Mallari <raynemllr.5@gmail.com>
2023-11-14 19:36:13 +0800

    Update techStackIcons data map

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,15 +1,9 @@
-"use client";
-
+import { Separator } from "@/components/ui/separator";
 import { personalLinks, techStackIcons } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
-import { Separator } from "@/components/ui/separator";
-import { useTheme } from "next-themes";
-import { techStackIcon, techStackIconsType } from "@/types";
 
 function Hero() {
-  const { theme } = useTheme();
-
   return (
     <section id="home" className="hero">
       <div className="section-container py-36">
@@ -56,24 +50,22 @@ function Hero() {
                 orientation="vertical"
               />
               <div className="mt-2 grid grid-cols-9 gap-x-3">
-                {techStackIcons.map(({ name, icon }: techStackIconsType) => (
+                {techStackIcons.map(({ name, icon }) => (
                   <Image
                     key={name}
-                    src={
-                      name === "nextjs"
-                        ? theme === "dark"
-                          ? (icon as techStackIcon).dark
-                          : (icon as techStackIcon).light
-                        : typeof icon === "string"
-                        ? icon
-                        : icon.light
-                    }
+                    src={icon}
                     alt={`${name}-icon`}
                     width={100}
                     height={100}
                     className="h-8 w-8"
                   />
                 ))}
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
+                  <path
+                    fill="currentColor"
+                    d="M64 0C28.7 0 0 28.7 0 64s28.7 64 64 64c11.2 0 21.7-2.9 30.8-7.9L48.4 55.3v36.6h-6.8V41.8h6.8l50.5 75.8C116.4 106.2 128 86.5 128 64c0-35.3-28.7-64-64-64zm22.1 84.6l-7.5-11.3V41.8h7.5v42.8z"
+                  />
+                </svg>
               </div>
             </div>
 
