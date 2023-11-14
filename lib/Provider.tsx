2139d162e0c7c5dfb62f13b5eb610c7aeaedd3ae
Rayne Mallari <raynemllr.5@gmail.com>
2023-11-14 14:50:26 +0800

    Remove RehydrationWrapper component

--- lib/Provider.tsx
+++ lib/Provider.tsx
@@ -7,21 +7,10 @@ type ProviderProps = {
   children: React.ReactNode;
 };
 
-const RehydrationWrapper = ({ children }: ProviderProps) => {
-  // * To avoid hydration issues
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => setMounted(true), []);
-  if (!mounted) return null;
-
-  return <>{children}</>;
-};
-
 export default function Provider({ children }: ProviderProps) {
   return (
-    <RehydrationWrapper>
-      <ThemeProvider enableSystem={true} attribute="class">
-        {children}
-      </ThemeProvider>
-    </RehydrationWrapper>
+    <ThemeProvider enableSystem={false} attribute="class">
+      {children}
+    </ThemeProvider>
   );
 }
